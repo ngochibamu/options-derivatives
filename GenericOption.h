79d795cffb6833d0d7fd4cd401fcbf10671795a6
ngochibamu <ngoni.chibamu@gmail.com>
2017-10-23 15:49:39 +0200

    GenericOption.cpp

---
+++ GenericOption.h
@@ -0,0 +1,28 @@
+#ifndef __CppOptions__GenericOption__
+#define __CppOptions__GenericOption__
+
+#include <stdio.h>
+
+enum OptionType {
+	OptionType_Call,
+	OptionType_Put
+};
+
+
+class GenericOption {
+
+public:
+	GenericOption(double strike, OptionType type, double cost);
+	GenericOption(const GenericOption &genericOption);
+	~GenericOption();
+	GenericOption &operator=(const GenericOption &genericOption);
+
+	double valueAtExpiration(double underLyingAtExpiration);
+	double profitAtExpiration(double underLyingAtExpiration);
+
+private:
+	double strike;
+	OptionType type;
+	double cost;
+}
+#endif;
